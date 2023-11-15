eb5b9aa9c0025f5554f80b4d2e84bde9dbd135ff
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-11-15 16:01:45 -0600

    updated seeds and models

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,9 +1,10 @@
 const db = require('./connection');
-const { User } = require('../models');
+const { User, Artwork } = require('../models');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   await cleanDB('User', 'users');
+  await cleanDB('Artwork', 'artwork');
 
 await User.create({
     firstName: 'Test',
@@ -21,5 +22,51 @@ await User.create({
 
   console.log('users seeded');
 
-  process.exit();
-});
+const artwork = await Artwork.insertMany([
+  {
+    title: 'Mona Lisa',
+    image: 'mona-lisa.jpg',
+    price: 860000000,
+    artist: 'Leonardo Da Vinci',
+  },
+  {
+    title: 'Starry Night',
+    image: 'starry-night.jpg',
+    price: 70000000,
+    artist: 'Vincent Van Gogh',
+  },
+  {
+    title: 'This Kiss',
+    image: 'this-kiss.jpg',
+    price: 36000,
+    artist: 'Gustav Klimt',
+  },
+  {
+    title: 'The School of Athens',
+    image: 'the-school-of-athens.webp',
+    price: 6000,
+    artist: 'Raphael',
+  },
+  {
+    title: 'Las Meninas',
+    image: 'las-meninas.jpg',
+    price: 4000000,
+    artist: 'Diego Velázquez',
+  },
+  {
+    title: 'The birth of Venus',
+    image: 'the-birth-of-venus.jpg',
+    price: 500000000,
+    artist: 'Sandro Botticelli',
+  },
+  {
+    title: 'Guernica',
+    image: 'guernica.jpg',
+    price: 200000000,
+    artist: 'Pablo Picasso',
+  },
+])
+
+})
+
+process.exit()
