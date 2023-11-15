198f347f70659fe07cc0d29bdb6c30b247d7c47c
Matt Estin <mattestin@gmail.com>
2023-11-15 13:38:40 -0500

    imgData files added to pull images to product slides

---
+++ client/src/assets/imageData.js
@@ -0,0 +1,26 @@
+import prodThumb1 from "./prod1-thumbnail.jpg"
+import prodThumb2 from "./prod2-thumbnail.jpg"
+import prodThumb3 from "./prod3-thumbnail.jpg"
+import prodThumb4 from "./prod4-thumbnail.jpg"
+import prodImg1 from "./prod1.jpg"
+import prodImg2 from "./prod2.jpg"
+import prodImg3 from "./prod3.jpg"
+import prodImg4 from "./prod4.jpg"
+
+const productImages = [
+    { url: prodImg1, alt: "product slide 1" },
+    { url: prodImg2, alt: "product slide 2" },
+    { url: prodImg3, alt: "product slide 3" },
+    { url: prodImg4, alt: "product slide 4" },
+]
+
+const productThumbnails = [
+    { url: prodThumb1, alt: "product thumbnail slide 1" },
+    { url: prodThumb2, alt: "product thumbnail slide 2" },
+    { url: prodThumb3, alt: "product thumbnail slide 3" },
+    { url: prodThumb4, alt: "product thumbnail slide 4" },
+]
+export { productImages, productThumbnails }
+
+
+// to use with stock product images and seeds
---
+++ server/utils/imgData.js
@@ -0,0 +1,13 @@
+import { productImages } from "../../client/src/assets/imageData"
+
+export const data = {
+    productId: 1,
+    companyName: "or artist / author/artwork",
+    productName: "monalisa",
+    productDescription:
+        "acrylic canvas painting",
+    productPrice: 250,
+
+    images: productImages,
+}
+// fill in with model data once it is created
