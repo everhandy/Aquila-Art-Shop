18fe00f13de22aa344972286fa750ddecc66d0b6
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-11-15 12:25:08 -0600

    updated seeds images

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -10,27 +10,45 @@ db.once('open', async () => {
 const artwork = await Artwork.insertMany([
   {
     title: 'Mona Lisa',
-    image: '',
+    image: 'mona-lisa.jpg',
     price: 860000000,
     artist: 'Leonardo Da Vinci',
   },
   {
     title: 'Starry Night',
-    image: '',
+    image: 'starry-night.jpg',
     price: 70000000,
     artist: 'Vincent Van Gogh',
   },
   {
     title: 'This Kiss',
-    image: '',
+    image: 'this-kiss.jpg',
     price: 36000,
     artist: 'Gustav Klimt',
   },
   {
     title: 'The School of Athens',
-    image: '',
+    image: 'the-school-of-athens.webp',
     price: 6000,
     artist: 'Raphael',
+  },
+  {
+    title: 'Las Meninas',
+    image: 'las-meninas.jpg',
+    price: 4000000,
+    artist: 'Diego Velázquez',
+  },
+  {
+    title: 'The birth of Venus',
+    image: 'the-birth-of-venus.jpg',
+    price: 500000000,
+    artist: 'Sandro Botticelli',
+  },
+  {
+    title: 'Guernica',
+    image: 'guernica.jpg',
+    price: 200000000,
+    artist: 'Pablo Picasso',
   },
 ])
 
