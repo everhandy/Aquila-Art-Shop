c18b33ecd6d9a93ab0afd2d1ddc76c3271382c3a
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-11-16 10:18:34 -0600

    updated id's

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -17,56 +17,62 @@ await User.create({
     firstName: 'Testtwo',
     lastName: 'Testertwo',
     email: 'testtwo@testmail.com',
-    password: 'password12345'
+    password: 'password12345',
   });
 
   console.log('users seeded');
 
 const artwork = await Artwork.insertMany([
   {
+    id: 1,
     title: 'Mona Lisa',
     image: 'mona-lisa.jpg',
     price: 860000000,
     artist: 'Leonardo Da Vinci',
   },
   {
+    id: 2,
     title: 'Starry Night',
     image: 'starry-night.jpg',
     price: 70000000,
     artist: 'Vincent Van Gogh',
   },
   {
+    id: 3,
     title: 'This Kiss',
     image: 'this-kiss.jpg',
     price: 36000,
     artist: 'Gustav Klimt',
   },
   {
+    id: 4,
     title: 'The School of Athens',
     image: 'the-school-of-athens.webp',
     price: 6000,
     artist: 'Raphael',
   },
   {
+    id: 5,
     title: 'Las Meninas',
     image: 'las-meninas.jpg',
     price: 4000000,
     artist: 'Diego Velázquez',
   },
   {
+    id: 6,
     title: 'The birth of Venus',
     image: 'the-birth-of-venus.jpg',
     price: 500000000,
     artist: 'Sandro Botticelli',
   },
   {
+    id: 7,
     title: 'Guernica',
     image: 'guernica.jpg',
     price: 200000000,
     artist: 'Pablo Picasso',
   },
 ])
+process.exit()
 
 })
-
-process.exit()
--- server/models/Artwork.js
+++ server/models/Artwork.js
@@ -1,29 +1,33 @@
 const mongoose = require('mongoose');
-const {INTEGER} = require('sequelize');
-const {Schema} = mongoose;
+const { INTEGER } = require('sequelize');
+const { Schema } = mongoose;
 
 const artworkSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
 
-    title: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    image: {
-      type: String
-    },
-    price: {
-      type: Number,
-      required: true,
-      min: 0.99
-    },
-    artist: {
-      type: String,
-      required: true,
-    },
-  
-  });
-  
-  const Artwork = mongoose.model('Artwork', artworkSchema);
-  
-  module.exports = Artwork;
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99
+  },
+  artist: {
+    type: String,
+    required: true,
+  },
+
+});
+
+const Artwork = mongoose.model('Artwork', artworkSchema);
+
+module.exports = Artwork;
