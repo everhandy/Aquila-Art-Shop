d029d6855d9b902ec665fd4b4d0744372d8b56f6
everhandy <everettdhandy@outlook.com>
2023-11-22 08:31:28 -0500

    added add to cart buttons which send to cart

--- client/src/components/Gallery/index.jsx
+++ client/src/components/Gallery/index.jsx
@@ -1,60 +1,95 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Isotope from 'isotope-layout';
 import ImagesLoaded from 'imagesloaded';
 import PropTypes from 'prop-types';
 import GalleryItemsData from '../../galleryItems';
+import { useStoreContext } from '../../utils/GlobalState';
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
-class Gallery extends Component {
-    componentDidMount() {
-        const galleryItemsName = this.grid;
-        const galleryItemName = '.gallery-item';
-
-        const iso = new Isotope(galleryItemsName, {
-            itemSelector: galleryItemName,
-            masonry: {
-                horizontalOrder: true
-            }
+const Gallery = (props) => {
+    const [state, dispatch] = useStoreContext();
+    const { cart } = state;
+    const galleryRef = useRef(null);
+  
+    const addToCart = (item) => {
+      if (!item._id) {
+        console.error('Item does not have an _id property');
+        return;
+        }
+      const itemInCart = cart.find((cartItem) => cartItem._id === item._id);
+      if (itemInCart) {
+        const newQuantity = parseInt(itemInCart.purchaseQuantity) + 1;
+        
+        dispatch({
+          type: UPDATE_CART_QUANTITY,
+          _id: item._id,
+          purchaseQuantity: newQuantity
         });
 
-        const imgLoad = new ImagesLoaded(galleryItemsName);
-
-        imgLoad.on('progress', function (instance, image) {
-            iso.layout();
+        idbPromise('cart', 'put', {
+          ...itemInCart,
+          purchaseQuantity: newQuantity
         });
-    }
+      } else {
+        dispatch({
+          type: ADD_TO_CART,
+          product: { ...item, purchaseQuantity: 1 }
+        });
+        idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+      }
+    };
+  
+    useEffect(() => {
+      const iso = new Isotope(galleryRef.current, {
+        itemSelector: '.gallery-item',
+        masonry: {
+          horizontalOrder: true
+        }
+      });
+  
+      const imgLoad = new ImagesLoaded(galleryRef.current);
+      imgLoad.on('progress', function () {
+        iso.layout();
+      });
+    }, []);
 
-    render() {
-        return (
-            <div className="gallery">
-                <div className="wrapper">
+    // Added to check if gallery items have an id passed through
+    GalleryItemsData.forEach(item => {
+        console.log(item._id); // This should log a unique ID for each item
+      });
+      
+  
+    return (
+      <div className="gallery">
+        <div className="wrapper">
+        </div>
+        <div className={"gallery-item-wrapper" + props.paddingBottomClass}>
+          <div className="gallery-items" ref={galleryRef}>
+            {GalleryItemsData && GalleryItemsData.map((item, key) => (
+              <div key={key} className="gallery-item active ">
+                <div className="img object-fit">
+                  <div className="object-fit-cover">
+                    <img src={item.imgLink} alt={item.title} />
+                  </div>
                 </div>
-                <div className={"gallery-item-wrapper" + this.props.paddingBottomClass}>
-                    <div className="gallery-items" ref={(c) => this.grid = c}>
-                        {GalleryItemsData && GalleryItemsData.map((item, key) => (
-                            <div key={key} className="gallery-item active ">
-                                <div className="img object-fit">
-                                    <div className="object-fit-cover">
-                                        <img src={item.imgLink} alt={item.title} />
-                                    </div>
-                                </div>
-
-                                <div className="gallery-hover">
-                                    <div className="gallery-hover-wrapper">
-                                        <h3>{item.title}</h3>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+  
+                <div className="gallery-hover">
+                  <div className="gallery-hover-wrapper">
+                    <h3>{item.title}</h3>
+                    <button onClick={() => addToCart(item)}>Add to cart</button>
+                  </div>
                 </div>
-            </div>
-        );
-    }
-}
-
-Gallery.propTypes = {
-    seeMore: PropTypes.string,
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  Gallery.propTypes = {
     paddingBottomClass: PropTypes.string
-};
-
-export default Gallery;
+  };
+  
+  export default Gallery;
