3da2ccc2fbb33ff830c5b13ab5a2ec2e592e6421
everhandy <everettdhandy@outlook.com>
2023-11-22 13:22:21 -0500

    added env and trying to debug to deploy

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,5 +1,12 @@
 module.exports = {
+  env: { browser: true, es2020: true },
   extends: ['react-app'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
